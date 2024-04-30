4aa1f6e2ddde888f4a3ca45f1205b21f36d14ef0
Sirius <romerperez26@gmail.com>
2024-04-29 23:10:32 -0400

    Agregando try catch a la practica 1

--- Practica1/Class/Calculadora.cs
+++ Practica1/Class/Calculadora.cs
@@ -1,6 +1,32 @@
 ﻿namespace Practica1.Class
 {
-    internal class Calculadora
+    public class Calculadora
     {
+        public void CalculadoraProducto()
+        {
+            //Variables
+            int num1 = 0;
+            int num2 = 0;
+            decimal suma = 0;
+            decimal producto = 0;
+
+            try
+            {
+                Console.WriteLine("Digite un numero: ");
+                num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite un numero: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                suma = (num1 + num2);
+                producto = (num1 * num2);
+
+                Console.WriteLine($"La suma de los dos numeros es: {suma} y su producto es {producto}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio el siguente error: {ex.Message}");
+            }
+        }
     }
 }
