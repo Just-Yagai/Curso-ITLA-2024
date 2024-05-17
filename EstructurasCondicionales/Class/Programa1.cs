e2039b320be58af319ac297780e48a5ed0fd1257
Sirius <romerperez26@gmail.com>
2024-05-17 12:23:08 -0400

    #Comment Programa1 modulo 2

---
+++ EstructurasCondicionales/Class/Programa1.cs
@@ -0,0 +1,25 @@
+﻿namespace EstructurasCondicionales.Class
+{
+    public class Programa1
+    {
+        public void CalcularPerimetro()
+        {
+            int lado = 0;
+            int perimetro = 0;
+
+            try
+            {
+                Console.WriteLine("Digite la longitud del lado del cuadrado: ");
+                lado = int.Parse(Console.ReadLine());
+
+                perimetro = lado * 4;
+
+                Console.WriteLine($"El perimetro del cuadrado es: { perimetro }");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio el siguente error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ EstructurasCondicionales/Program.cs
@@ -0,0 +1,5 @@
+﻿using EstructurasCondicionales.Class;
+
+Programa1 estructurasCond = new Programa1();
+
+estructurasCond.CalcularPerimetro();
